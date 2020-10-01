da41c75137a61ddf7e556cbc9b1d5f58a7799489
Emanuel Samana <e.samana@assistsa.com.ar>
2020-10-01 13:17:37 -0300

    New Controller and View Clientes

---
+++ CMRmvc/Controllers/ClientesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMRmvc.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Crud(bool isreadonly, string myaction, long? id)
+        {
+            ViewBag.IsReadOnly = isreadonly;
+            ViewBag.Action = myaction;
+
+
+            return View();
+        }
+    }
+}
