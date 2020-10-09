36f92a59714ac63b3a4caea1ceaeba698265ea6e
Emanuel Samana <e.samana@assistsa.com.ar>
2020-10-09 10:56:10 -0300

    ..

--- CMRmvc/Controllers/EmailController.cs
+++ CMRmvc/Controllers/EmailController.cs
@@ -25,18 +25,22 @@ namespace CMRmvc.Controllers
             {
                 EmailViewModel email = new EmailViewModel();
                 email.Id = i;
-                email.Destinatario = string.Format("ejemplo{0}@email.com", i);
-                email.Asunto = "Asunto del mensaje " + i;
-                email.Mensaje = "Cuerpo del mensaje " + i ;
+                email.Destinatario = string.Format("ejemplo{0}@email.com", i);     
                 email.Fec = DateTime.Now.AddDays(-i);
 
                 if ((i % 2) == 0)
                 {
+                    email.Asunto = "El pasaje estándar Lorem Ipsum, usado desde el año 1500.";
+                    var mimsg = "Lorem Ipsum is simply dummy text of the printing and typesetting\n industry. Lorem Ipsum has been the industry's standard dummy\ntext ever since the 1500s, when an unknown printer took a galley\n of type and scrambled it to make a type specimen book. It has\n survived not only five centuries, but also the leap into electronic\n typesetting, remaining essentially unchanged. It was popularised in\n the 1960s with the release of Letraset sheets containing Lorem\n Ipsum passages, and more recently with desktop publishing\n software like Aldus PageMaker including versions of Lorem Ipsum";
+                    email.Mensaje = mimsg + "  ForID:" + i;
                     email.TypeCorreo = TypeCorreo.Recibido;
                     email.Etiqueta = EtiquetaCorreo.Importante;
                 }
                 else
                 {
+                    email.Asunto = "Sección 1.10.33 de Finibus Bonorum et Malorum, escrito por Cicero en el 45 antes de Cristo";
+                    var mimsg = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis\n praesentium voluptatum deleniti atque corrupti quos dolores\n et quas molestias excepturi sint occaecati cupiditate non provident,\n similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.\nEt harum quidem rerum facilis est et expedita distinctio.Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.\n Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae\n sint et molestiae non recusandae.Itaque earum rerum hic tenetur a sapiente delectus,\n ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.";
+                    email.Mensaje = mimsg + "  ForID:" + i;
                     email.TypeCorreo = TypeCorreo.Enviado;
                     email.Etiqueta = EtiquetaCorreo.Promociones;
                 }
@@ -90,7 +94,7 @@ namespace CMRmvc.Controllers
 
         public ActionResult Inbox(TypeCorreo mytype)
         {
-            StartMethod();
+            StartMethod(); 
             try
             {
                 ViewBag.TitleName = mytype;
